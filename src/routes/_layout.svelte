5ae2e49ad6ddc3e6f03ccfd470c77a9b28679cd6
Svobodin <Ivan.Svobodin@emerson.com>
2020-12-19 15:30:19 +0500

    try

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,16 @@
 <script>
 	import Nav from "../components/Nav.svelte";
-
+	import Canvas from "../components/canvas.svelte";
 	export let segment;
 </script>
 
 <style>
+	.canvas {
+		position: fixed;
+		width: 100vw;
+		height: 100vh;
+		background-color: green;
+	}
 	main {
 		position: relative;
 		background-color: black;
@@ -13,6 +19,9 @@
 </style>
 
 <Nav {segment} />
+<div class="canvas">
+	<Canvas bind:page={segment} />
+</div>
 
 <main>
 	<slot />
