37337a03fb60fb6c358ee4e0abd2460d9bd66ed2
Svobodin <Ivan.Svobodin@emerson.com>
2021-01-15 11:25:20 +0500

    all worck preloader and animation

---
+++ src/routes/test.svelte
@@ -0,0 +1,39 @@
+<style>
+    /* .svg-clipped {
+        -webkit-clip-path: url(#svgPath);
+       
+    } */
+    .holder {
+        width: 400px;
+        height: 250px;
+        clip-path: url(#svgPath);
+    }
+    .color {
+        width: 100%;
+        height: 20%;
+        background-color: violet;
+    }
+</style>
+
+<div class="holder">
+    <div class="color" />
+    <!-- <img class="svg-clipped" src="flowers.jpg" alt="Cherry Blossoms." /> -->
+</div>
+
+<svg height="0" width="0">
+    <defs>
+        <clipPath
+            clipPathUnits="objectBoundingBox"
+            transform="scale(0.00125, 0.002)"
+            id="svgPath">
+            <path
+                fill="#FFFFFF"
+                stroke="#FFFFFF"
+                stroke-width="1.5794"
+                stroke-miterlimit="10"
+                d="M215,100.3c97.8-32.6,90.5-71.9,336-77.6
+        c92.4-2.1,98.1,81.6,121.8,116.4c101.7,149.9,53.5,155.9,14.7,178c-96.4,54.9,5.4,269-257,115.1c-57-33.5-203,46.3-263.7,20.1
+        c-33.5-14.5-132.5-45.5-95-111.1C125.9,246.6,98.6,139.1,215,100.3z" />
+        </clipPath>
+    </defs>
+</svg>
